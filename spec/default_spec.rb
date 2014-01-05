0441d494559bab777d753266d76208e26ee51c44
Doc Walker <doc.walker@jameshardie.com>
2014-01-05 17:00:16 -0600

    Fix chefspec deprecations

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -27,7 +27,7 @@ describe 'ntp::default' do
   
   it 'should create /etc/cron.hourly/ntpdate owned by root:root' do
     file = '/etc/cron.hourly/ntpdate'
-    chef_run.should create_file_with_content file, 'node.file.header'
+    expect(chef_run).to render_file(file).with_content('node.file.header')
     expect(chef_run.template(file).owner).to eq('root')
     expect(chef_run.template(file).group).to eq('root')
   end # it 'should create /etc/cron.hourly/ntpdate owned by root:root'
