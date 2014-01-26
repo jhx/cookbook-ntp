0f53ace801cc5c99c87ef82ca09ff18cd8be3558
Doc Walker <doc.walker@jameshardie.com>
2014-01-26 12:01:08 -0600

    Fix #24 add support for test-kitchen

---
+++ test/integration/ntp/serverspec/ntp_spec.rb
@@ -0,0 +1,44 @@
+# coding: utf-8
+require 'spec_helper'
+
+describe 'ntp::default' do
+  context package('ntp') do
+    it 'is installed' do
+      expect(subject).to be_installed
+    end # it
+  end # context
+
+  context service('ntp') do
+    it 'is not running' do
+      expect(subject).to_not be_running
+    end # it
+
+    it 'is not enabled' do
+      expect(subject).to_not be_enabled
+    end # it
+  end # context
+
+  context file('/etc/cron.hourly/ntpdate') do
+    it 'is file' do
+      expect(subject).to be_file
+    end # it
+
+    it 'is owned by root' do
+      expect(subject).to be_owned_by('root')
+    end # it
+
+    it 'is in group root' do
+      expect(subject).to be_grouped_into('root')
+    end # it
+
+    it 'is mode 755' do
+      expect(subject).to be_mode(755)
+    end # it
+
+    it 'matches expected content' do
+      expect(subject.content)
+        .to match('ntpdate fake.time.server >> /dev/null 2>&1')
+    end # it
+  end # context
+
+end # describe
